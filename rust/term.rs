3fcf8bb0e2f30ca6e79f8b98f3a32df3572a9534
vernisaz <javaarchitect@msn.com>
2026-03-16 19:44:49 -0700

    formatted

--- rust/term.rs
+++ rust/term.rs
@@ -1,56 +1,67 @@
+extern crate simcolor;
 extern crate simterm;
 extern crate simweb;
-extern crate simcolor;
 use std::{
     collections::HashMap,
-    path::PathBuf, env::{self,consts}, process::Command,
+    env::{self, consts},
+    path::{PathBuf,MAIN_SEPARATOR_STR},
+    process::Command,
 };
 
-use simterm::{Terminal, VERSION as TERM_VERSION};
 use simcolor::Colorized;
+use simterm::{Terminal, VERSION as TERM_VERSION};
 
 const VERSION: &str = env!("VERSION");
 
 struct Commander;
 
 impl Terminal for Commander {
-    fn init(&self) -> (PathBuf, PathBuf, HashMap<String,Vec<String>>,&str) {
+    fn init(&self) -> (PathBuf, PathBuf, HashMap<String, Vec<String>>, &str) {
         unsafe { env::set_var("COLORTERM", "truecolor") } // since terminal can be invoked from a service (not a regular terminal session)
         let web = simweb::WebData::new();
-        let os_drive =
-            if "windows" == consts::OS {
-                env::var("SystemDrive").unwrap_or_default()
-            } else {
-                 String::new()
-            };
+        let os_drive = if "windows" == consts::OS {
+            env::var("SystemDrive").unwrap_or_default()
+        } else {
+            String::new()
+        };
         let cwd = match web.param("cwd") {
             Some(cwd) => PathBuf::from(cwd),
-            _ => PathBuf::from(format!("{os_drive}{}", std::path::MAIN_SEPARATOR_STR))
+            _ => PathBuf::from(format!("{os_drive}{}", MAIN_SEPARATOR_STR)),
         };
         let mut aliases = HashMap::new();
-        if let Some(mut home_dir) = env::home_dir () {
+        if let Some(mut home_dir) = env::home_dir() {
             home_dir.push(".beerc.7b");
             if home_dir.is_file() {
                 let output = Command::new("rb")
                     .arg("-f")
-                     .arg(home_dir.display().to_string())
-                     .current_dir(&cwd)
-                     .output();
+                    .arg(home_dir.display().to_string())
+                    .current_dir(&cwd)
+                    .output();
                 if let Ok(output) = output {
-                    for (key,val) in String::from_utf8_lossy(&output.stdout).lines()
-                        .filter_map(|line| line.split_once('=')) { 
+                    for (key, val) in String::from_utf8_lossy(&output.stdout)
+                        .lines()
+                        .filter_map(|line| line.split_once('='))
+                    {
                         if let Some(alias) = key.strip_prefix("alias ") {
                             if val.len() > 2 {
-                                aliases.insert(alias.to_string(), val.trim_matches('\'').split_whitespace().map(str::to_string).collect());
+                                aliases.insert(
+                                    alias.to_string(),
+                                    val.trim_matches('\'')
+                                        .split_whitespace()
+                                        .map(str::to_string)
+                                        .collect(),
+                                );
                             }
                         } else {
-                            unsafe { env::set_var(key,val); }
+                            unsafe {
+                                env::set_var(key, val);
+                            }
                         }
                     }
                 }
             }
         }
-        (cwd.clone(),cwd,aliases,VERSION)
+        (cwd.clone(), cwd, aliases, VERSION)
     }
     fn greeting(&self, version: &str) -> String {
         let ver = version.color_num(196).to_string();
